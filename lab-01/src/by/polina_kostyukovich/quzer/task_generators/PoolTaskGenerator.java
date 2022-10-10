b5a5179e58212d42e1d6870bfa2dbdd0e975a21b
polyushka <polyushka.kostyukovich@gmail.com>
2022-10-10 18:38:33 +0300

    fix: Fix PoolTaskGenerator

--- lab-01/src/by/polina_kostyukovich/quzer/task_generators/PoolTaskGenerator.java
+++ lab-01/src/by/polina_kostyukovich/quzer/task_generators/PoolTaskGenerator.java
@@ -2,7 +2,9 @@ package by.polina_kostyukovich.quzer.task_generators;
 
 import by.polina_kostyukovich.quzer.tasks.Task;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Random;
 
 public class PoolTaskGenerator implements Task.Generator {
@@ -13,14 +15,25 @@ public class PoolTaskGenerator implements Task.Generator {
 
     public PoolTaskGenerator(boolean allowDuplicate, Task... tasks) {
         this.allowDuplicate = allowDuplicate;
-        this.tasks = tasks;
-        randomIndexes = createRandomIndexes(tasks.length);
+        if (allowDuplicate) {
+            this.tasks = tasks;
+            randomIndexes = null;
+        } else {
+            this.tasks = getTasksWithoutDuplicate(tasks);
+            randomIndexes = createRandomIndexes(this.tasks.length);
+        }
     }
 
     public PoolTaskGenerator(boolean allowDuplicate, Collection<Task> tasks) {
         this.allowDuplicate = allowDuplicate;
-        this.tasks = tasks.toArray(new Task[0]);
-        randomIndexes = createRandomIndexes(this.tasks.length);
+        Task[] tasksArray = tasks.toArray(new Task[0]);
+        if (allowDuplicate) {
+            this.tasks = tasksArray;
+            randomIndexes = null;
+        } else {
+            this.tasks = getTasksWithoutDuplicate(tasksArray);
+            randomIndexes = createRandomIndexes(this.tasks.length);
+        }
     }
 
     private static int[] createRandomIndexes(int size) {
@@ -37,6 +50,34 @@ public class PoolTaskGenerator implements Task.Generator {
             indexes[i] = temp;
         }
         return indexes;
+    }
+
+    private static Task[] getTasksWithoutDuplicate(Task[] tasks) {
+        if (tasks.length == 0) {
+            return new Task[0];
+        }
+
+        Arrays.sort(tasks, Comparator.comparing(Task::getText));
+        Task current_task = tasks[0];
+        int numberOfUnique = 1;
+        for (Task task : tasks) {
+            if (!current_task.getText().equals(task.getText())) {
+                ++numberOfUnique;
+                current_task = task;
+            }
+        }
+
+        Task[] uniqueTasks = new Task[numberOfUnique];
+        int current_index = 0;
+        current_task = tasks[0];
+        for (Task task : tasks) {
+            if (!current_task.getText().equals(task.getText())) {
+                ++current_index;
+                current_task = task;
+                uniqueTasks[current_index] = task;
+            }
+        }
+        return uniqueTasks;
     }
 
     @Override
