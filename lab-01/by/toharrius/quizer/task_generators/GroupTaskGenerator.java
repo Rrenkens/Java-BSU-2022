e69ab12603dca0708c522460d5a274e99bd0561c
toharrius <antonkharasau@gmail.com>
2022-11-22 23:03:51 +0300

    Implement GroupTaskGenerator

---
+++ lab-01/by/toharrius/quizer/task_generators/GroupTaskGenerator.java
@@ -0,0 +1,44 @@
+package by.toharrius.quizer.task_generators;
+
+import by.toharrius.quizer.Task;
+import by.toharrius.quizer.TaskGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
+
+class GroupTaskGenerator implements TaskGenerator {
+    ArrayList<TaskGenerator> generators;
+    /**
+     * Конструктор с переменным числом аргументов
+     *
+     * @param generators генераторы, которые в конструктор передаются через запятую
+     */
+    GroupTaskGenerator(TaskGenerator... generators) {
+        this.generators = new ArrayList<>();
+        Collections.addAll(this.generators, generators);
+    }
+
+    /**
+     * Конструктор, который принимает коллекцию генераторов
+     *
+     * @param generators генераторы, которые передаются в конструктор в Collection (например, {@link ArrayList})
+     */
+    GroupTaskGenerator(Iterable<TaskGenerator> generators) {
+        this.generators = new ArrayList<>();
+        for (var gen : generators) {
+            this.generators.add(gen);
+        }
+    }
+
+    /**
+     * @return результат метода generate() случайного генератора из списка.
+     *         Если этот генератор выбросил исключение в методе generate(), выбирается другой.
+     *         Если все генераторы выбрасывают исключение, то и тут выбрасывается исключение.
+     */
+    public Task generate() {
+        var r = ThreadLocalRandom.current();
+        int index = r.nextInt(generators.size());
+        return generators.get(index).generate();
+    }
+}
