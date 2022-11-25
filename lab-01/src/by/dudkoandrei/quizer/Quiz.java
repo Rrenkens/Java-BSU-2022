9a57b7da527b76603267601d85d643868d84450b
DudkoAndrei <dudkoandrei@gmail.com>
2022-11-25 17:08:16 +0300

    Implement Quiz methods

--- lab-01/src/by/dudkoandrei/quizer/Quiz.java
+++ lab-01/src/by/dudkoandrei/quizer/Quiz.java
@@ -7,12 +7,29 @@ import by.dudkoandrei.quizer.tasks.Task;
  */
 class Quiz {
 
+  private final Task.Generator taskGenerator;
+
+  private Task currentTask = null;
+  private final int taskCount;
+  private boolean answeredOnCurrentTask = false;
+
+  private int correctAnswerNumber = 0;
+  private int wrongAnswerNumber = 0;
+  private int incorrectInputNumber = 0;
+
   /**
    * @param generator генератор заданий
    * @param taskCount количество заданий в тесте
    */
   Quiz(Task.Generator generator, int taskCount) {
-    // ...
+    if (taskCount <= 0) {
+      // exception
+    }
+    if (generator == null) {
+      // exception
+    }
+    this.taskGenerator = generator;
+    this.taskCount = taskCount;
   }
 
   /**
@@ -20,7 +37,16 @@ class Quiz {
    * @see Task
    */
   Task nextTask() {
-    // ...
+    if (isFinished()) {
+      // exception
+    }
+
+    if (answeredOnCurrentTask || currentTask == null) {
+      currentTask = taskGenerator.generate();
+      answeredOnCurrentTask = false;
+    }
+
+    return currentTask;
   }
 
   /**
@@ -29,35 +55,53 @@ class Quiz {
    * самый объект {@link Task}.
    */
   Result provideAnswer(String answer) {
-    // ...
+    if (isFinished()) {
+      // exception
+    }
+
+    Result result = currentTask.validate(answer);
+
+    switch (result) {
+      case OK -> {
+        correctAnswerNumber++;
+        answeredOnCurrentTask = true;
+      }
+      case WRONG -> {
+        wrongAnswerNumber++;
+        answeredOnCurrentTask = true;
+      }
+      case INCORRECT_INPUT -> incorrectInputNumber++;
+    }
+
+    return result;
   }
 
   /**
    * @return завершен ли тест
    */
   boolean isFinished() {
-    // ...
+    return correctAnswerNumber + wrongAnswerNumber == taskCount;
   }
 
   /**
    * @return количество правильных ответов
    */
   int getCorrectAnswerNumber() {
-    // ...
+    return correctAnswerNumber;
   }
 
   /**
    * @return количество неправильных ответов
    */
   int getWrongAnswerNumber() {
-    // ...
+    return wrongAnswerNumber;
   }
 
   /**
    * @return количество раз, когда был предоставлен неправильный ввод
    */
   int getIncorrectInputNumber() {
-    // ...
+    return incorrectInputNumber;
   }
 
   /**
@@ -65,6 +109,10 @@ class Quiz {
    * вопросов. Оценка выставляется только в конце!
    */
   double getMark() {
-    // ...
+    if (!isFinished()) {
+      // exception
+    }
+
+    return ((double) correctAnswerNumber) / taskCount;
   }
 }
