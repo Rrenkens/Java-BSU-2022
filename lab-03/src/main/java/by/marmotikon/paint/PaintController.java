80aa2373f3a9aff68c7daee97b32291041ad3511
Marmotikon <msemenov93057@gmail.com>
2022-11-26 14:11:06 +0300

    Mark up the application interface

--- lab-03/src/main/java/by/marmotikon/paint/PaintController.java
+++ lab-03/src/main/java/by/marmotikon/paint/PaintController.java
@@ -1,14 +1,29 @@
 package by.marmotikon.paint;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PaintController {
     @FXML
-    private Label welcomeText;
+    private TextField brushSize;
+    @FXML
+    private ColorPicker brushColor;
+    @FXML
+    private CheckBox eraser;
+    @FXML
+    private Canvas canvas;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSave() {
+        System.err.println("Save");
+    }
+
+    @FXML
+    protected void onExit() {
+        System.err.println("Exit");
     }
 }
