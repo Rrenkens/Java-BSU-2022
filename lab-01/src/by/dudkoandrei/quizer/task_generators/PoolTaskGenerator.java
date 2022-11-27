32cd8fbdb8bf56345da7abfeea85958bcc413530
DudkoAndrei <dudkoandrei@gmail.com>
2022-11-27 18:18:36 +0300

    Add PoolTaskGenerator class

---
+++ lab-01/src/by/dudkoandrei/quizer/task_generators/PoolTaskGenerator.java
@@ -0,0 +1,70 @@
+package by.dudkoandrei.quizer.task_generators;
+
+import by.dudkoandrei.quizer.tasks.Task;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Task.Generator, который отдает задания из заранее заготовленного набора.
+ */
+public class PoolTaskGenerator implements Task.Generator {
+
+  private final ArrayList<Task> tasks;
+  private final boolean allowDuplicate;
+  private int currentPosition = 0;
+  private static final Random rnd = new Random();
+
+  /**
+   * Конструктор с переменным числом аргументов.
+   *
+   * @param allowDuplicate разрешить повторения
+   * @param tasks          задания, которые в конструктор передаются через запятую
+   */
+  PoolTaskGenerator(boolean allowDuplicate, Task... tasks) {
+    this(allowDuplicate, Arrays.asList(tasks));
+  }
+
+  /**
+   * Конструктор, который принимает коллекцию заданий.
+   *
+   * @param allowDuplicate разрешить повторения
+   * @param tasks          задания, которые передаются в конструктор в Collection (например,
+   *                       {@link LinkedList})
+   */
+  PoolTaskGenerator(boolean allowDuplicate, Collection<Task> tasks) {
+    if (tasks == null) {
+      // exception
+    }
+    if (tasks.isEmpty()) {
+      // exception
+    }
+    if (tasks.contains(null)) {
+      // exception
+    }
+
+    this.allowDuplicate = allowDuplicate;
+    this.tasks = new ArrayList<>(tasks);
+
+    if (!allowDuplicate) {
+      Collections.shuffle(this.tasks);
+    }
+  }
+
+  /**
+   * @return случайная задача из списка
+   */
+  public Task generate() {
+    if (allowDuplicate) {
+      return tasks.get(rnd.nextInt(tasks.size()));
+    }
+
+    if (currentPosition == tasks.size()) {
+      // exception;
+    }
+
+    return tasks.get(currentPosition++);
+  }
+}
