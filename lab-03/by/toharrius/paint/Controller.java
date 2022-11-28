9224555b1f039ae8bc1eb13f9a25b721231ee167
shandomruffle <antonkharasau@gmail.com>
2022-11-28 18:53:43 +0300

    Show colors to choose from

--- lab-03/by/toharrius/paint/Controller.java
+++ lab-03/by/toharrius/paint/Controller.java
@@ -2,18 +2,18 @@ package by.toharrius.paint;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class Controller {
     private static Controller instance = null;
+    public FlowPane colorBoxFlow;
     private PaintingTool paintingTool = PaintingTool.PENCIL;
     public HBox root;
     public Canvas mainCanvas;
-    public VBox sidePane;
-
     public Controller() {
         instance = this;
     }
@@ -22,6 +22,17 @@ public class Controller {
         var ctx = mainCanvas.getGraphicsContext2D();
         ctx.setFill(Color.WHITE);
         ctx.fillRect(0, 0, mainCanvas.getWidth(), mainCanvas.getHeight());
+
+        for (var color : new String[]{
+                "white", "black", "grey",
+                "red", "green", "blue",
+                "yellow", "hotpink", "lightblue"
+        }) {
+            var box = new Label();
+            box.getStyleClass().add("color-box");
+            box.setStyle("-fx-background-color: " + color);
+            colorBoxFlow.getChildren().add(box);
+        }
     }
     public void mousePressed(MouseEvent mouseEvent) {
         switch (paintingTool) {
