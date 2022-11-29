b95523bc73f45f97af5b5f0fe3c92a994fd26576
DaniilDomnin <fpm.domnin@bsu.by>
2022-11-29 17:37:18 +0300

    Fix

--- lab-02/DocksAndHobos/src/by/DaniilDomnin/doks_and_hobos/Main.java
+++ lab-02/DocksAndHobos/src/by/DaniilDomnin/doks_and_hobos/Main.java
@@ -15,8 +15,8 @@ import org.json.simple.parser.ParseException;
 public class Main {
 
     public static void main(String[] args) throws IOException, ParseException, InterruptedException {
-       // Object obj = new JSONParser().parse(new FileReader(args[1]));
-        Object obj = new JSONParser().parse(new FileReader("config.json"));
+        Object obj = new JSONParser().parse(new FileReader(args[1]));
+        // Object obj = new JSONParser().parse(new FileReader("config.json"));
         JSONObject jo = (JSONObject) obj;
         ShipGenerator generator = new ShipGenerator((long)jo.get("ship_capacity_max"), (long)jo.get("ship_capacity_min"), (ArrayList<String>) jo.get("cargo_types"));
         Docks docks = new Docks((long)jo.get("dock_capacity"), (long)jo.get("unloading_speed"),  (ArrayList<String>) jo.get("cargo_types"), (long)jo.get("hobos"),   (ArrayList<Long>) jo.get("ingredients_count"), (long)jo.get("eating_time"), (long)jo.get("stealing_time"));
