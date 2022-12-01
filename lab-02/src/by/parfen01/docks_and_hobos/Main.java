bafb049db009eb8f5196f456df46a3cff5652863
parfen01 <alex889986@gmail.com>
2022-12-01 17:52:12 +0300

    Micro fix

--- lab-02/src/by/parfen01/docks_and_hobos/Main.java
+++ lab-02/src/by/parfen01/docks_and_hobos/Main.java
@@ -10,9 +10,10 @@ import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException, ParseException {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("wrong number of arguments");
+        }
         String path = args[0];
-        // я знаю, что нулевой аргуент это обычно вся строка, а аргументы для команды начинаются с 1,
-        // просто в идее проще сразу прописать внутри программы путь нулевым аргументом
         Controller controller = ProjectInitializer.initController(path);
         controller.start();
         sleep(10 * 1000L);
