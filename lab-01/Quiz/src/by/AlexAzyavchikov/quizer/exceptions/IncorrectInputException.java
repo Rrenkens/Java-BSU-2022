548537b1bfa1bac2c3f5f9395cba17f3c604931c
AlexAzyavchikov <azyavchikov.alex@gmail.com>
2022-12-06 22:57:43 +0300

    leave only lab3

--- lab-01/Quiz/src/by/AlexAzyavchikov/quizer/exceptions/IncorrectInputException.java
+++
@@ -1,7 +0,0 @@
-package by.AlexAzyavchikov.quizer.exceptions;
-
-public class IncorrectInputException extends RuntimeException {
-    public IncorrectInputException(String message) {
-        super(message);
-    }
-}
--- lab-01/Quiz/src/by/AlexAzyavchikov/quizer/exceptions/QuizNotFinishedException.java
+++
@@ -1,7 +0,0 @@
-package by.AlexAzyavchikov.quizer.exceptions;
-
-public class QuizNotFinishedException extends RuntimeException {
-    public QuizNotFinishedException(String message) {
-        super(message);
-    }
-}
